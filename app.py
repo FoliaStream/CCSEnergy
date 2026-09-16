e739ff6ceb089268bc152abe03be0120660dbe91
FoliaStream <foliastream@gmail.com>
2026-09-16 10:09:35 +0700

    start_api

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from flask import Flask, jsonify, request
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.read_csv("data.csv")
+
+@app.route("/")
+def home():
+    msg = jsonify({"message": "My CSV API is running!"})
